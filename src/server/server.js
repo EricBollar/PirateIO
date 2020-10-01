91c1c9d52830226d1267562e4d1a550a0e80272b
EricBollar <esbollar@gmail.com>
2020-09-30 18:32:09 -0700

    trying to remove port

--- src/server/server.js
+++ src/server/server.js
@@ -21,9 +21,9 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Listen on port
-const port = process.env.PORT || 3000;
-const server = app.listen(port);
-console.log(`Server listening on port ${port}`);
+//const port = process.env.PORT || 3000;
+const server = app.route('/');
+//console.log(`Server listening on port ${port}`);
 
 // Setup socket.io
 const io = socketio(server);
